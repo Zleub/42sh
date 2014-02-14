e7e7a0b0ffc795e0a24e1abaad99e31384d202ef
Noe Sierra-Velasquez <noesv42@gmail.com>
2014-02-14 12:29:00 +0100

    Clean basic things. Will be agremented shortly.

---
+++ nsierra-/env.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/14 10:30:29 by nsierra-          #+#    #+#             */
+/*   Updated: 2014/02/14 10:31:34 by nsierra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENV_H
+# define ENV_H
+
+/*
+** Specify a size and returns a char ** filled with NULL of size + 1.
+** Returns NULL on failure.
+*/
+char			**env_create_new(size_t size);
+/*
+** Copies src into dst. If dst is NULL, then it will be malloc'd. Otherwise,
+** make sure dst is big enough. Returns NULL on failure.
+*/
+char			**env_copy(const char **src, char **dst);
+/*
+** Looks for the variable ID specified by var (case-sensitive). Returns var
+** index if var is found. Otherwise, it returns -1.
+*/
+int				env_is_set(const char **env, const char *var);
+/*
+** Returns the size of the NULL-terminated array pointed by env.
+*/
+size_t			env_get_size(const char **env);
+/*
+** Creates a new env, copies the old one into it and adds the couple var + val
+** at the end of the array. On successfull, returns a pointer to the new env
+** and frees the old one. Returns NULL on failure and does not free the old
+** env.
+*/
+char			**env_add_var(char **env, const char *var, const char *value);
+/*
+** Looks for the index of the variable specified by var into env. On
+** successfull, frees the old variable, sets a new couple var + val at
+** this very index and returns the index.
+** Returns -1 on failure.
+*/
+int				env_update_var(char **env, const char *var, const char *value);
+void			env_destroy(char **env);
+
+#endif /* !ENV_H */
---
+++ nsierra-/env2.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env2.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsierra- <nsierra-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/14 11:26:16 by nsierra-          #+#    #+#             */
+/*   Updated: 2014/02/14 11:26:17 by nsierra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "env.h"
+
+static char		*gen_var(const char *var, const char *value, const char del)
+{
+	int			new_var_size;
+	char		*new_var;
+	char		*keep;
+
+	new_var_size = 0;
+	new_var_size += ft_strlen(var) + ft_strlen(value) + 1;
+	if (!(new_var = malloc(sizeof(char) * new_var_size + 1)))
+		return (NULL);
+	keep = new_var;
+	while (*var != '\0')
+	{
+		*new_var = *var++;
+		++new_var;
+	}
+	*new_var = del;
+	while (*value != '\0')
+	{
+		*new_var = *value++;
+		++new_var;
+	}
+	*new_var = '\0';
+	return (keep);
+}
+/*
+** Creates a new env, copies the old one into it and adds the couple var + val
+** at the end of the array. On successfull, returns a pointer to the new env
+** and frees the old one. Returns NULL on failure and does not free the old
+** env.
+*/
+char			**env_add_var(char **env, const char *var, const char *value)
+{
+	char		**new_env;
+	char		*new_var;
+	int			old_env_size;
+
+	old_env_size = env_get_size(env);
+	if (!(new_env = NULL)
+		&& (!(new_env = env_create_new(old_env_size + 1)))
+		|| !env_copy(env, new_env) || !(new_var = gen_var(var, value, '=')))
+		return (NULL);
+	new_env[old_env_size] = new_var;
+	new_env[old_env_size + 1] = NULL;
+	env_destroy(env);
+	return (new_env);
+}
+/*
+** Looks for the index of the variable specified by var into env. On
+** successfull, frees the old variable, sets a new couple var + val at
+** this very index and returns the index.
+** Returns -1 on failure.
+*/
+int				env_update_var(char **env, const char *var, const char *value)
+{
+	char		*new_var;
+	int			var_index;
+
+	if ((var_index = env_is_set(var)) < 0
+		|| !(new_var = gen_var(var, value, '=')))
+		return (-1);
+	free(env[var_index]);
+	env[var_index] = new_var;
+	return (var_index);
+}
